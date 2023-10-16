f1a7964ce4d8024b773f91098cab04f94a645698
flogea <katua.logvinova@gmail.com>
2023-10-16 19:35:11 +0300

    homework 2

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './Form.scss';
+import Post from '../Post';
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
+    this.inputRef = React.createRef();
     this.state = {
       inputValue: '',
+      btnStatus: false,
+      posts: [
+        'hi there! I`m Katua',
+        'I`m 22 y. o.',
+        'I`m a 4th year student at SPbSUT',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ipsum id consequuntur sapiente, quibusdam necessitatibus sed nihil dicta! Tempora minima ab deleniti inventore, deserunt officia ea numquam fugiat eligendi aliquam.',
+      ],
     };
   }
 
+  handleFocusOnInput = (event) => {
+    event.preventDefault();
+    this.inputRef.current.focus();
+  };
+
   handleChange = (event) => {
-    this.setState({ inputValue: event.target.value });
+    const inpValue = event.target.value;
+    this.setState({ inputValue: inpValue });
+    inpValue === 'реакт' ? this.setState({ btnStatus: true }) : this.setState({ btnStatus: false });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
     console.log('Form submitted with value:', this.state.inputValue);
     this.setState({ showModal: true });
+    this.setState((prevState) => ({
+      posts: [...prevState.posts, this.state.inputValue],
+    }));
     this.setState({ inputValue: '' });
   };
 
@@ -42,9 +61,23 @@ class Form extends React.Component {
             value={this.state.inputValue}
             onChange={this.handleChange}
             placeholder="Enter the value..."
+            ref={this.inputRef}
           />
-          <button type="submit">Submit</button>
+          <div className="buttons">
+            <button className="inverted" onClick={this.handleFocusOnInput}>
+              Focus
+            </button>
+            <button type="submit" disabled={this.state.btnStatus}>
+              Submit
+            </button>
+          </div>
         </form>
+
+        {this.state.posts.map((post, index) => (
+          <React.Fragment>
+            <Post post={post} key={`${post}_${index}`} />
+          </React.Fragment>
+        ))}
       </>
     );
   }
---
+++ src/components/Post/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Post.scss';
+
+function Post({ post }) {
+  return <div className="post">{post}</div>;
+}
+
+export default Post;
